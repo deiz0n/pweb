2f3fe4a1026470b5baaa3915840911af16d5f392
Carlos Eduardo SIlva <80070966+deiz0n@users.noreply.github.com>
2023-12-18 19:44:40 -0300

    Criação do projeto

---
+++ pweb-2/react/test/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hello, Dudu
+    </>
+  )
+}
+
+export default App
